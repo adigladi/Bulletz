709b1d5b815b8ef8674461d1c8e91327c7be90f7
adigladi <adigladi@hotmail.com>
2020-03-03 01:10:29 +0100

    chore: add tailwind and clean up unused files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
 import React from "react";
-import logo from "./logo.svg";
-import FetchSHL from "./components/fetchSHL";
-import "./App.css";
+import TeamSelect from "./components/TeamSelect";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <FetchSHL />
+      <TeamSelect />
     </div>
   );
 }
